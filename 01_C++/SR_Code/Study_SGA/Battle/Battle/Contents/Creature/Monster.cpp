7b45000f7d945c55670a9c92dac6b7ad89110a0f
yeonsungrok <36717739+yeonsungrok@users.noreply.github.com>
2024-09-03 09:52:24 +0900

    업로드2

---
+++ 01_C++/SR_Code/Study_SGA/Battle/Battle/Contents/Creature/Monster.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "Monster.h"
+
+Monster::Monster(string name, int hp, int atk)
+: Creature(name, hp, atk)
+{
+}
+
+Monster::~Monster()
+{
+}
+
+//void Monster::Attack(Creature* other)
+//{
+//	this->Creature::PreAttack(other);
+//
+//	other->TakeDamage(_atk, this);
+//}
+
+void Monster::Attack(shared_ptr<Creature> other)
+{
+	this->Creature::PreAttack(other);
+
+	auto self_shared = shared_from_this();
+	other->TakeDamage(_atk, self_shared);
+}
